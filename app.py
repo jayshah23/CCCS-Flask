7eef60107757c152f715b7833956f20a19105670
jayshah23 <58947890+jayshah23@users.noreply.github.com>
2021-08-12 10:18:19 +0530

    UI & formatting

--- app.py
+++ app.py
@@ -8,32 +8,10 @@ app = Flask(__name__)
 def welcome():
     return render_template('index.html')
 
-# @app.route('/model1',methods=['POST'])
-# def model():
-#     model = pickle.load(open('Model_Jay.pkl', 'rb'))
-#     # final_data = np.array([[129.84,1433.99,0.00,4000.0,1363.13,141.65,0.88]])
-#     final_data = [np.array([x for x in request.form.values()])]
-#     prediction = model.predict(final_data)
-
-#     if (prediction[0] == 0):
-#         response = "Customers of cluster 0 have a pretty good credit limit and balance but these customers have a very low oneoff purchase and purchases in comparison to cash advance. This means they take more cash advance and spend less through purchasing. These customers should be given schemes accordingly to increase their purchase which will indirectly increase oneoff purchase and installment purchases."
-#     elif (prediction[0] == 1):
-#         response = "Credit limit of customers of cluster 1 is lowest amongst all. These customers have a low balance but they have quite high purchase and installment purchases. These customers have a very low oneoff purchase and cash advance."
-#     elif (prediction[0] == 2):
-#         response = "These customers have a good credit limit but their balance, oneoff urchase, purchases, installment purchases is very low. So these customers should be encouraged by giving schemes to spend more and to increase the balance by keeping a minimum balance"
-#     elif (prediction[0] == 3):
-#         response = "These customers have the highest credit limit and oneoff purchase followed by purchases and installment purchases. They have highest payment of all clusters. These customers should be encouraged to keep decent balance in their account by keeping a minimum balance rule."
-#     else:
-#         response = "Error"
-
-#     return render_template('index.html', output=f'{response}')
-
 @app.route('/model',methods=['POST'])
 def model():
     model1 = pickle.load(open('Model_Jay.pkl', 'rb'))
     model2 = pickle.load(open('model_JINI.pkl', 'rb'))
-
-    # final_data = np.array([[3049.22,1.00,390.00,390.00,0.00,5876.87,9,1,3200.0,6537.91,2729.08,0.00,12]])
     
     final_data = [np.array([x for x in request.form.values()])]
     prediction1 = model1.predict([final_data[0][:7]])
